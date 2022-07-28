221f94147b34b8a17bee21f4ba9ad72b95756d48
Luara <luaraap7@gmail.com>
2022-07-28 18:47:52 -0300

    exercicio2/finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let array1 = [2, 1, 5, 9, 4, 8, 7, 6];
+console.log("Quantidade de itens Array 1:", array1.length)
+
+
+let array2 = ["amor", "felicidade,", "esperança"];
+console.log("Quantidade de itens Array 2:", array2.length)
+
+
+let array3 = [28, 44, "garrafa", false]
+console.log("Quantidade de itens Array 3:", array3.length)
