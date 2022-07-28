26955ae590532b43fd84362fe92649a0c8280835
Luara <luaraap7@gmail.com>
2022-07-28 19:21:39 -0300

    exercicio3/finalizdo

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,10 +1,18 @@
 let array1 = [2, 1, 5, 9, 4, 8, 7, 6];
 console.log("Quantidade de itens Array 1:", array1.length)
+console.log("Primeiro Item do Primeiro Array:", array1[0])
+console.log (array1.includes(7))
+
 
 
 let array2 = ["amor", "felicidade,", "esperança"];
-console.log("Quantidade de itens Array 2:", array2.length)
+console.log("Quantidade de itens Array 2:", array2)
+console.log("Segundo Item do Segundo Array:", array2[1])
+
 
 
 let array3 = [28, 44, "garrafa", false]
-console.log("Quantidade de itens Array 3:", array3.length)
+console.log("Quantidade de itens Array 3:", array3)
+console.log("Terceiro Item do Terceiro Array:", array3[2])
+console.log(array3.includes("relógio"))
+
---
+++ exercicio3/script.js
@@ -0,0 +1,4 @@
+let arrayOriginal1 = ["casa", "sapato", "relógio"]
+console.log("Original", arrayOriginal1)
+let copiaArray1 = arrayOriginal1.slice()
+console.log("Cópia", copiaArray1.push("teclado"))
