e00913c502d9e0db1d9668a480e1f5a4f1c90e77
Luara <luaraap7@gmail.com>
2022-07-28 17:24:02 -0300

    exercicio1/finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let array1 = [2, 1, 5, 9, 4, 8, 7, 6];
+console.log(array1)
+
+let array2 = ["amor", "felicidade,", "esperança"];
+console.log(array2)
+
+let array3 = [28, 44, "garrafa", false]
+console.log(array3)
